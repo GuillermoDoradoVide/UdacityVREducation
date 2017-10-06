7e0b643a70f5df0972a2d275e75bc74884bfa99c
Guillermo Dorado Vide <guillermo.dorado@telefonica.net>
2017-10-06 16:42:21 +0200

    Project folder structure. First scene hierarchy distribution.

---
+++ UdacityVREducation/Assets/Code/UI/DisplayInfoController_UI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayInfoController_UI : MonoBehaviour {
+
+    public Text displayText;
+
+	private void Start () {
+        displayText = transform.GetComponentInChildren<Text>();
+        if (displayText == null)
+        {
+            Debug.LogWarning("(" + transform.name + ") couldn't find a child text component.");
+            Debug.Break();
+        }
+	}
+
+    public void setDisplayInfoText(string newText)
+    {
+        displayText.text = newText;
+    }
+}
