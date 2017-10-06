3b4290d19f5fcc7a0d2a88c8ffcbc4a0bd42ef02
Guillermo Dorado Vide <guillermo.dorado@telefonica.net>
2017-10-06 17:29:58 +0200

    UI displayTextInfo sample with a simple show/Hide lerp animation with alpha.

--- UdacityVREducation/Assets/Code/UI/DisplayInfoController_UI.cs
+++ UdacityVREducation/Assets/Code/UI/DisplayInfoController_UI.cs
@@ -5,19 +5,62 @@ using UnityEngine.UI;
 
 public class DisplayInfoController_UI : MonoBehaviour {
 
+    public Transform mainCanvas;
     public Text displayText;
 
+    private CanvasGroup mainCanvasCanvasGroup;
+    private bool isUIAnimationTransition;
+
 	private void Start () {
-        displayText = transform.GetComponentInChildren<Text>();
+        mainCanvas = transform.GetChild(0);
+        displayText = mainCanvas.GetComponentInChildren<Text>();
         if (displayText == null)
         {
             Debug.LogWarning("(" + transform.name + ") couldn't find a child text component.");
             Debug.Break();
         }
-	}
+        mainCanvasCanvasGroup = mainCanvas.GetComponent<CanvasGroup>();
+        if (mainCanvasCanvasGroup == null)
+        {
+            Debug.LogWarning("(" + mainCanvas.name + ") couldn't find canvas group component.");
+            Debug.Break();
+        }
+        isUIAnimationTransition = false;
+    }
 
     public void setDisplayInfoText(string newText)
     {
         displayText.text = newText;
+    }
+    [ContextMenu("ShowUI")]
+    public void show()
+    {
+        showHideDisplayInfo(1);
+    }
+
+    [ContextMenu("HideUI")]
+    public void hide()
+    {
+        showHideDisplayInfo(0);
+    }
+
+    public void showHideDisplayInfo( float action, float speed = 1f)
+    {
+        isUIAnimationTransition = false;
+        StartCoroutine(DisplayInfoVisibility(speed, action));
+    }
+
+    private IEnumerator DisplayInfoVisibility(float speed, float action)
+    {
+        float t = 0;
+        float currentAlphaValue = mainCanvasCanvasGroup.alpha;
+        isUIAnimationTransition = true;
+        while (mainCanvasCanvasGroup.alpha != action && isUIAnimationTransition)
+        {
+            t += speed * Time.deltaTime;
+            mainCanvasCanvasGroup.alpha = Mathf.Lerp(currentAlphaValue, action, t);
+            yield return null;
+        }
+        isUIAnimationTransition = false;
     }
 }
